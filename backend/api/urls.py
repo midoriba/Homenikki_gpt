fe464eedc6620d9955aee6796e4a8e9491137116
midorikawa <48616869+midoriba@users.noreply.github.com>
2023-06-17 11:08:21 +0900

    静的ファイル向け設定追加

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from rest_framework import routers
 from DiaryPost.views import DiaryViewSet
+from django.views.generic import TemplateView, RedirectView
 
 defaultRouter = routers.DefaultRouter()
 defaultRouter.register('diary', DiaryViewSet, basename='diary')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('api/', include(defaultRouter.urls)),
     path('api/auth/',include('djoser.urls')),
     path('api/auth/',include('djoser.urls.jwt')),
+    re_path('', RedirectView.as_view(url='/'))
 ]
